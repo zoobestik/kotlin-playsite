f62bf58a91a678568f9f88b792ccdb7d5d9a72a7
zoobestik <kb.chernenko@gmail.com>
2023-01-16 11:45:09 +0100

    fix; link available test is broken on webkit+linux for 308 codes

--- src/utils/specs/asserts/links/available.ts
+++ src/utils/specs/asserts/links/available.ts
@@ -10,6 +10,27 @@ const OK_CODES = [200];
 const REDIRECT_CODES = [301, 302, 307, 308];
 const SOFT_CODES_CHECK = [...OK_CODES, ...REDIRECT_CODES];
 
+/* Domain should be replaced by target domain, check it in
+ * playwright next versions. Cause, webkit on linux is not rise events:
+ *   [ `request`, `response` ] or `.fetch()` in `route`.
+ */
+const HTTP_CODE_308_DOMAIN_LIST: { [domain: string]: string } = {
+  'jetbrains.com': 'www.jetbrains.com',
+};
+
+function fixExceptionUrl(page: Page, uri: string) {
+  const browser = page.context().browser();
+  const isWebkit = browser && browser.browserType().name() === 'webkit';
+
+  const url = new URL(uri);
+
+  if (isWebkit && url.hostname in HTTP_CODE_308_DOMAIN_LIST) {
+    url.hostname = HTTP_CODE_308_DOMAIN_LIST[url.hostname];
+  }
+
+  return url.toString();
+}
+
 async function assertAvailable<T extends Page, K extends ExternalLink>(
   page: T,
   link: K,
@@ -19,8 +40,9 @@ async function assertAvailable<T extends Page, K extends ExternalLink>(
   ) => Promise<[Response, () => Promise<void> | null | undefined]>,
 ) {
   const linkUrl = await link.getAttribute('href');
-  if (!linkUrl) throw Error('Link is empty');
-  const normalizeUrl = new URL(linkUrl).toString();
+  if (!linkUrl) throw new Error('Link is empty');
+
+  const normalizeUrl = fixExceptionUrl(page, new URL(linkUrl).toString());
 
   const [response, finalize] = await getResponse(normalizeUrl);
 
@@ -40,24 +62,15 @@ export function assertAvailableBlank(
   options: AvailableOptions,
 ) {
   return assertAvailable(page, link, options, async (originalUrl) => {
-    const newPagePromise = page.context().waitForEvent('page');
+    const [response, newPage] = await Promise.all([
+      page.context().waitForEvent('response', {
+        predicate: (page) => page.url() === originalUrl,
+      }),
+      page.context().waitForEvent('page'),
+      link.click(),
+    ]);
 
-    try {
-      const [newPage, response] = await Promise.all([
-        newPagePromise,
-        page.context().waitForEvent('response', {
-          predicate: (response: Response) => originalUrl === response.url(),
-        }),
-        link.click(),
-      ]);
-
-      return [response, () => newPage.close()];
-    } catch (e) {
-      // Close page if error in response
-      const newPage = await newPagePromise.catch((e) => Promise.resolve(e));
-      if (newPage && newPage.close) await newPage.close();
-      throw e;
-    }
+    return [response, () => newPage.close()];
   });
 }
 
@@ -77,7 +90,8 @@ export function assertAvailableInline(
     return [
       response,
       async () => {
-        await page.goto(currentUrl);
+        if (currentUrl !== page.url())
+          await page.goto(currentUrl, { waitUntil: 'domcontentloaded' });
       },
     ];
   });
@@ -88,13 +102,19 @@ export async function assertLinkAvailable(
   link: ExternalLink,
   options?: AvailableOptions,
 ) {
-  const isBlank = (await link.getAttribute('target')) === '_blank';
+  const attribute = await link.getAttribute('target');
+  const isBlank = attribute === '_blank';
   const asserTester = isBlank ? assertAvailableBlank : assertAvailableInline;
-  return asserTester(page, link, options || {});
+  await asserTester(page, link, options || {});
+  return;
 }
 
-export async function assertLinksAvailable(page: Page, links: Locator) {
-  for (const link of await links.all()) {
-    await assertLinkAvailable(page, link);
+export async function assertLinksAvailable(
+  page: Page,
+  container: Locator,
+  options?: AvailableOptions,
+) {
+  for (const link of await container.all()) {
+    await assertLinkAvailable(page, link, options);
   }
 }
